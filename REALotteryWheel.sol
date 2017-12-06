15dd2b8fcf2b06b64c87549413ce086e7a4a7ebe
pavel metelitsyn <metelitsyn@outlook.com>
2017-12-06 14:14:04 +0100

    comments

--- REALotteryWheel.sol
+++ REALotteryWheel.sol
@@ -1,8 +1,37 @@
 pragma solidity ^0.4.19;
+/*
+	REA Lottery Wheel Contract
+
+	The constructor sets last_hash to some initial value.
+	Every call to spin() will increase the round_count by one and
+	a put a new "random" hash into the storage map "hashes".
+	spin() accepts an argument which can be used to introdue more "randomness".
+
+	The community can participate by sending small amounts of Eth (no matter the value)
+	to the smart contract. The value sent together with timestamp and blocknumber increase
+	the "randomness".
+
+	The outcome of round <n> can be retrived via call to get_hash(<n>).  
+
+	WARNING and DISCLAIMER: 
+	We fully understand the fact that Ethereum Smart Contracts
+	by design of Ethereum Blockchain and Solidity language work
+	in a determenistic and predictable way. 
+
+	The block number and the timestamp are not random variables in
+	a mathematical sense. Even worse, the interested miners can 
+	affect the outcome by not including the contract transaction
+	in a current block if they are not happy about the outcome 
+	(since miners in theory know the outcome of every contract transaction
+	before the transaction is included in a block). 
+
+	2017 Pavel Metelitsyn
+
+*/
 
 contract REALotteryWheel{
     
-    uint16 public last_round = 0;
+    uint16 public round_count = 0;
     bytes32 public last_hash;
     address public controller;
     
@@ -15,9 +44,9 @@ contract REALotteryWheel{
     
     function spin(bytes32 s) public {
         if(controller != msg.sender) revert();
-        last_round = last_round + 1;
+        round_count = last_round + 1;
         last_hash = keccak256(block.number,now,s);
-        hashes[last_round] = last_hash;
+        hashes[round_count] = last_hash;
         
     }
     
